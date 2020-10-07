148a9a198653023c78edc86000a082142ec4f763
Welton Santos <weltonsantos@dcc.ufmg.br>
2020-10-07 11:01:34 -0300

    Commit inicial No ramo master
    Submissão inicial.
    
    Mudanças a serem submetidas:
    	new file:   .gitignore
    	new file:   dados/cond-mat-2003.gml
    	new file:   dados/cond-mat-2003.txt
    	new file:   dados/cond-mat-2003.zip
    	new file:   dados/cond-mat-2005.gml
    	new file:   dados/cond-mat-2005.txt
    	new file:   dados/cond-mat-2005.zip
    	new file:   dados/cond-mat.gml
    	new file:   dados/cond-mat.txt
    	new file:   dados/cond-mat.zip
    	new file:   dados/polblogs.gml
    	new file:   dados/polblogs.txt
    	new file:   dados/polblogs.zip
    	new file:   dados/polbooks.gml
    	new file:   dados/polbooks.txt
    	new file:   dados/polbooks.zip
    	new file:   grafo.py
    	new file:   pip-r.txt
    	new file:   req.txt

---
+++ grafo.py
@@ -0,0 +1,59 @@
+import igraph
+
+class Grafo():
+
+    def __init__(self):
+
+        #self.grafo = igraph.read('dados/cond-mat.gml')
+        #self.grafo = igraph.read('dados/polblogs.gml')
+        self.grafo = igraph.read('dados/polbooks.gml')
+
+if __name__=='__main__':
+
+    g = Grafo()
+    print("Número de vértices: ", g.grafo.vcount())
+    print("Número de arestas: ", g.grafo.ecount())
+    print("Densidade do grafo: ", g.grafo.density())
+    #print("Grau dos vértices: ", g.grafo.degree())
+    print("Grau médio dos 10 primeiros vértices (método usado: igraph.Graph().knn()): ")
+    for i, j in zip(g.grafo.knn()[0][:10], g.grafo.knn()[1][:10]):
+        print(i," - ", j)
+    print("Grau médio de clusterização da rede: ", g.grafo.transitivity_undirected())
+    
+    print("\nCalculando a centralidade dos vértices: ")
+    lista = []
+    for v, i in zip(g.grafo.vs, g.grafo.eigenvector_centrality()):
+        lista.append([v['label'], i])
+    
+    lista.sort(key=lambda x: x[1], reverse=True)
+    for i in lista[:20]:
+        print(i[0], i[1])
+
+    print("Imprimindo a rede...")
+    # Calculando o grau de cada vértice.
+    cont = 0
+    for i in g.grafo.degree():
+        g.grafo.vs[cont]["size"] = 10+i
+        cont += 1
+    # Layouts testados para melhorar o plot.
+    #layout = g.grafo.layout_reingold_tilford()
+    #layout = g.grafo.layout_lgl()
+    #layout = g.grafo.layout_circle()
+    layout = g.grafo.layout_kamada_kawai()
+    #"""
+    try:
+        igraph.plot(g.grafo,"rede.pdf", layout=layout, bbox = (2500, 2500))
+    except:
+        igraph.plot(g.grafo, layout=layout).save("grafo.png")
+    #"""
+
+    print("\nCalculando o pagerank da rede (Google Page Rank): ")
+    lista = []
+    for v, i in zip(g.grafo.vs, g.grafo.pagerank()):
+        lista.append([v['label'], i])
+    
+    lista.sort(key=lambda x: x[1], reverse=True)
+    
+    for i in lista[:20]:
+        print(i[0], i[1])
+    
